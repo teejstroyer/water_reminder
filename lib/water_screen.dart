9310696f3bdb3568ffcc12beb2d0d19ed2a81067
teejstroyer <titoriano@gmail.com>
2022-03-03 19:45:40 -0600

    cleaning up UI, opting for popup

--- lib/water_screen.dart
+++ lib/water_screen.dart
@@ -9,10 +9,13 @@ class WaterScreen extends StatefulWidget {
 }
 
 class _WaterScreenState extends State<WaterScreen> {
-  double ouncesDrunk = 0;
-  double maxOunces = 240;
   final Color waterColor = const Color(0xFF1E90FF);
   final double waveHeight = 100;
+
+  final int _maxOunces = 240;
+
+  int _ouncesDrunk = 580;
+  int _ouncesInput = 24;
 
   @override
   Widget build(BuildContext context) {
@@ -21,22 +24,39 @@ class _WaterScreenState extends State<WaterScreen> {
       appBar: AppBar(title: const Text('Water Reminder')),
       body: LayoutBuilder(builder: (context, constraints) {
         var maxHeight = constraints.biggest.height - waveHeight;
-        int height = ((ouncesDrunk / maxOunces) * maxHeight).clamp(0, maxHeight).toInt();
-        return Stack(
-          children: [
-            getWater(height),
-          ],
-        );
+        int height = ((_ouncesDrunk / _maxOunces) * maxHeight)
+            .clamp(0, maxHeight - 10)
+            .toInt();
+        return Stack(children: [
+          getWater(height),
+          Align(
+						alignment:Alignment.center,
+            child: Column(
+							mainAxisSize: MainAxisSize.min,
+              children: [
+                Row(
+							mainAxisSize: MainAxisSize.min,
+                  children: [
+											Column(children: [
+												Text(_ouncesDrunk.toString(), style: const TextStyle(color: Colors.black, fontSize: 50),),
+												Text(_maxOunces.toString(), style: const TextStyle(color: Colors.black, fontSize: 50),)
+											],),
+                    Text(
+                      ((_ouncesDrunk / _maxOunces) * 100).toStringAsFixed(0) + "%",
+                      style: const TextStyle(color: Colors.black, fontSize: 105),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          )
+        ]);
       }),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          setState(() {
-            ouncesDrunk += 20;
-          });
-        },
-        child: const Icon(Icons.add),
-      ),
     );
+  }
+
+  void addWater(bool positive) {
+    setState(() => _ouncesDrunk += (positive ? 1 : -1) * _ouncesInput);
   }
 
   Column getWater(int height) {
